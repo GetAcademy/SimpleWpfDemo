7436460b22bba43afb5bce5e2856c1374713f291
GET Academy <terje@getacademy.no>
2023-06-01 13:01:08 +0200

    Initial commit

---
+++ SimpleWpfDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleWpfDemo
+{
+    internal class Program
+    {
+        private static Label label;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            var button = new Button {Content = "Trykk her!"};
+            button.Click += ButtonClick;
+            label = new Label {Content = "0"};
+            var panel = new StackPanel();
+            panel.Children.Add(button);
+            panel.Children.Add(label);
+            window.Content = panel;
+            app.Run(window);
+        }
+
+        private static void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            //count++;
+            label.Content = "Du har trykket";
+        }
+    }
+}
+
+
+
